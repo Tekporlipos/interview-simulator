604dd8a49820be4589806e58dbbaaa88d52e71ea
tekporlipos <dzikunujohn36@gmail.com>
2024-03-29 15:48:09 +0000

    hotfix:db env fix

--- backend/instance/config.py
+++ backend/instance/config.py
@@ -2,5 +2,6 @@ import os
 
 
 class Config:
-    db_url = os.environ.get('POSTGRES_COPY_URL_TEST')
+    db_url = os.environ.get('POSTGRES_COPY_URL_TEST') if os.environ.get(
+        'VERCEL_ENV') == "development" else os.environ.get('POSTGRES_COPY_URL')
     flask_env = os.environ.get('VERCEL_ENV')
